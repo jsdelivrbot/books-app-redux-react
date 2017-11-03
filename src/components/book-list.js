b20ee76178fc6fc7519756276768b96cbedc521f
alex-vn <alex.vukovity@gmail.com>
2017-11-03 21:18:24 +0100

    DEVELOP: Creating the redux container

---
+++ src/components/book-list.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class BookList extends Component {
+    render() {
+        return (
+            <ul className="list-group col-sm-4">
+            
+            </ul>
+
+        )
+    }
+}
